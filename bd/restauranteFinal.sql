5843b2f999fa6c201662ccc3c55e929fcfed2378
PedroS <pedro.sandoval.martinez@ces-vegamedia.es>
2024-11-26 10:21:24 +0100

    sql mod

--- bd/restauranteFinal.sql
+++ bd/restauranteFinal.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `restaurante`
 --
-DELETE DATABASE IF EXISTS restaurante;
+DROP DATABASE IF EXISTS restaurante;
 CREATE DATABASE IF NOT EXISTS `restaurante` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `restaurante`;
 
